c5ae447a2e72973108a8a4829e338e60625862d0
adam.metal <adam.metal@codecool.com>
2023-07-27 10:21:12 +0200

    add login logout flow

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,7 +1,17 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 import { Outlet } from "react-router-dom";
+import { useAuth } from "../context/AuthProvider";
+import { useSignOut } from "react-firebase-hooks/auth";
+import auth from "../auth";
 
 function Layout() {
+  const user = useAuth();
+  const [signOut, loading] = useSignOut(auth);
+
+  const handleLogout = () => {
+    signOut();
+  };
+
   return (
     <>
       <AppBar position="static">
@@ -9,6 +19,16 @@ function Layout() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Chatify
           </Typography>
+
+          {user ? (
+            <Button
+              disabled={loading}
+              onClick={handleLogout}
+              variant="contained"
+            >
+              Logout
+            </Button>
+          ) : null}
         </Toolbar>
       </AppBar>
       <Outlet />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,49 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import Button from "@mui/material/Button";
+import Paper from "@mui/material/Paper";
+import auth from "../auth";
+
 function Login() {
+  const navigate = useNavigate();
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
+  useEffect(() => {
+    if (user) {
+      navigate("/chat");
+    }
+  }, [user, navigate]);
+
+  const handleLogin = () => {
+    signInWithGoogle();
+  };
+
+  if (error) {
+    return (
+      <Paper
+        elevation={4}
+        sx={{ m: 4, p: 4, display: "flex", justifyContent: "center" }}
+      >
+        Something went wrong
+      </Paper>
+    );
+  }
+
   return (
-    <h1>Login</h1>
-  )
+    <Paper
+      elevation={4}
+      sx={{ m: 4, p: 4, display: "flex", justifyContent: "center" }}
+    >
+      {!loading ? (
+        <Button variant="contained" onClick={handleLogin}>
+          Login
+        </Button>
+      ) : (
+        "In progress"
+      )}
+    </Paper>
+  );
 }
 
 export default Login;
